82cc3521a26bd6bc053d945c32d6cffc0e91d45e
SorrawitRUNGRUJEE <sorrawit_r@outlook.com>
2023-12-27 10:50:17 +0700

    finish:react_conditional_context_challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [isBodyOpen,setIsBodyOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [color,setColor] = useState(false)
+  const [bodyColor,setBodyColor] = useState(false)
+
+  const setModal = () => {
+      setIsModalOpen(!isModalOpen);
+    }
+  
+  const setColors=()=> setColor(!color)
+  
+  const setBody = () => setIsBodyOpen(!isBodyOpen)
+
+
+  const setBodyColors = () => setBodyColor(!bodyColor)
+  
+
+  const modal_content = {
+    title:"modal title",
+    sub_title:"modal sub title"
+  }
+
   return (
-    <h1>Hello</h1>
+    <div>
+      <div>
+        <button onClick={setBody}>trigger Body</button>
+        <button onClick={setBodyColors}>body color!</button>
+        <button onClick={setModal}>trigger Modal!</button>
+        <button onClick={setColors}>modal color!</button>
+      </div>
+       <Body bodyColor={bodyColor} body={isBodyOpen} modal={modal_content} title="where is the modal ?" content="here it is!" isModalOpen={isModalOpen} color={color} />
+    </div>
   );
+}
+
+const Body = ({bodyColor,title,content,isModalOpen,color,modal,body}) => {
+  return(
+<>{
+
+  body &&
+<div className={bodyColor ? "body":"bodyBefore"}>
+  <h1>this is body, not modal</h1>
+    <h1>{title}</h1>
+    {isModalOpen&& <Modal color={color} modal={modal} content={content} />}
+  </div>}
+
+
+
+</>
+  )
+   
+};
+
+const Modal = ({color,modal,content}) =>{
+  
+  
+  return(
+    <><div className={color? "modal":"modalBefore"}>
+
+    <h1>{content}</h1>
+      <h3>{modal.title}</h3>
+      <h3>{modal.sub_title}</h3>
+    </div>
+    </>
+    
+
+  )
 }
 
 export default App;
