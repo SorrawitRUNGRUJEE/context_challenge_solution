6449456bff6fab2de3d396db3e39af9aef8f70dd
SorrawitRUNGRUJEE <sorrawit_r@outlook.com>
2023-12-27 12:39:47 +0700

    init setup for react counter context challenge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+
+  const [score,setScore] = useState({
+    one:0,
+    two:0,
+    three:0,
+    four:0
+  })
+  
   return (
-    <h1>Hello</h1>
+    <>
+    </>
   );
 }
+
+
+const ScoreCard = () =>{
+  return(<div>
+
+  </div>)
+}
+
+const PlayerOne = ({name}) =>{
+  return (
+    <div>
+      <span> this is {name}</span>
+
+    </div>
+  )
+}
+const PlayerTwo = ({name}) =>{
+  return (
+    <div>
+      <span> this is {name}</span>
+
+    </div>
+  )
+}
+const PlayerThree = ({name}) =>{
+  return (
+    <div>
+      <span> this is {name}</span>
+
+    </div>
+  )
+}
+const PlayerFour = ({name}) =>{
+  return (
+    <div>
+      <span> this is {name}</span>
+
+    </div>
+  )
+}
+
+
 
 export default App;
